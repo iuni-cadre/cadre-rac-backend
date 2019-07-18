4582210a67e389938aa3f76ccb2d760baf9abc8a
Aditya Gupta <contactadityag@gmail.com>
2019-07-18 16:27:17 -0400

    Making changes to the script so that it works in one go.

--- sample.py
+++ sample.py
@@ -1,60 +1,34 @@
-import docker
-import tarfile
 import os
-import requests
-from docker import Client
-from io import BytesIO
+import docker
 
-os.chdir(r"/home/aditya/Documents/myimages5")
-
-#Create Tar file
-tarFile = tarfile.open("Dockerfile.tar.gz", 'w:gz')
-
-files = os.listdir(".")
-for f in files:
-    tarFile.add(f)
-
-#List files in tar
-for f in tarFile.getnames():
-    print("Added %s" % f)
-
-tarFile.close()
-
-headers = {
-    'Content-Type': 'application/tar',
+client = docker.DockerClient(base_url='tcp://127.0.0.1:2375')
+volumes= ['/home/aditya/Documents/docker']
+volume_bindings = {
+                    '/home/aditya/Documents/docker': {
+                        'bind': '/shared',
+                        'mode': 'rw',
+                    },
 }
 
-params = (
-    ('t', 'sample_test'),
-)
-
-data = open('Dockerfile.tar.gz', 'rb').read()
 # We are building the docker image from the dockerfile here
-try:
-    response = requests.post('http://localhost:2375/build?t=sample_test', headers=headers, params=params, data=data)
-except requests.exceptions.RequestException as e:
-    print('Exception caught:', e)
-
+image = client.images.build(path='/home/aditya/Documents/myimages5/', tag='sample_test', forcerm=True)
+print(os.getcwd())
+print("The image has been built successfully. ")
 # Create a container and run a command in the container
-dockerClient = Client(base_url='http://localhost:2375')
-container = dockerClient.create_container(image='sample_test', command='python script.py /home/aditya/Documents/myimages5/CadreQueryResult/guptaadi/ef28311e-aba1-4e82-badf-7761ad7c659d.csv')
-response = dockerClient.start(container)
-print(container)
-
-# Copy the output of the python script
-# dockerClient.copy('/usr/local/bin/output.txt', '~/output.txt') 
-
-# raw_stream,status = dockerClient.get_archive(container, '/usr/local/bin')
-# print(status)
-# tar_archive = BytesIO(b"".join((i for i in raw_stream)))
-# t = tarfile.open(mode='r:', fileobj=tar_archive)
-# text_from_container_file = t.extractfile('output.txt').read().decode('utf-8')
+container = client.containers.run('sample_test',
+                                  detach=True,
+                                  volumes={os.getcwd(): {'bind':'/tmp/', 'mode':'rw'}},
+                                  command='python script.py /home/aditya/Documents/myimages5/CadreQueryResult/guptaadi/ef28311e-aba1-4e82-badf-7761ad7c659d.csv',
+                                  remove=True)
+print(container.logs())
 print('The output of the file has been copied successfully outside the docker container')
 
-# Delete the container
-# dockerClient.remove_container(container, force = 'true')
+# Delete the docker container
 print('The container has been removed successfully.')
 
 # Delete the docker image
-# dockerClient.remove_image('sample_test', force = 'true')
+client.images.remove('sample_test', force=True)
 print('The image has been removed successfully.')
+
+# Deleting the unused images
+client.images.prune()
