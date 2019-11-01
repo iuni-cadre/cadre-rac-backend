1c7970598ea4b049c77e1b30bba940b985a34785
chathuriw <kamalasini@gmail.com>
2019-11-01 14:39:15 -0400

    adding kubernates

--- kub_config.py
+++ kub_config.py
@@ -1,36 +1,18 @@
 from kubernetes import client, config
 
+from kubernetes import client, config
+
 
 def main():
-    # Define the barer token we are going to use to authenticate.
-    # See here to create the token:
-    # https://kubernetes.io/docs/tasks/access-application-cluster/access-cluster/
-    aToken = "<token>"
+    # Configs can be set in Configuration class directly or using helper
+    # utility. If no argument provided, the config will be loaded from
+    # default location.
+    config.load_kube_config()
 
-    # Create a configuration object
-    aConfiguration = client.Configuration()
-
-    # Specify the endpoint of your Kube cluster
-    aConfiguration.host = "https://XXX.XXX.XXX.XXX:443"
-
-    # Security part.
-    # In this simple example we are not going to verify the SSL certificate of
-    # the remote cluster (for simplicity reason)
-    aConfiguration.verify_ssl = False
-    # Nevertheless if you want to do it you can with these 2 parameters
-    # configuration.verify_ssl=True
-    # ssl_ca_cert is the filepath to the file that contains the certificate.
-    # configuration.ssl_ca_cert="certificate"
-
-    aConfiguration.api_key = {"authorization": "Bearer " + aToken}
-
-    # Create a ApiClient with our config
-    aApiClient = client.ApiClient(aConfiguration)
-
-    # Do calls
-    v1 = client.CoreV1Api(aApiClient)
+    v1 = client.CoreV1Api()
     print("Listing pods with their IPs:")
     ret = v1.list_pod_for_all_namespaces(watch=False)
+    v1.create_namespaced_pod()
     for i in ret.items:
         print("%s\t%s\t%s" %
               (i.status.pod_ip, i.metadata.namespace, i.metadata.name))
